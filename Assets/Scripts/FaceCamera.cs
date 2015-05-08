df9254cc6f26c1c909998a832de751fbf2622597
Yuri van Geffen <yuri@yurivangeffen.com>
2015-05-09 00:55:07 +0200

    Added scruipt for facing camera and made it work

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,32 @@
+﻿//FaceCamera.cs v02
+//by Neil Carter (NCarter)
+//modified by Juan Castaneda (juanelo)
+//
+//added in-between GRP object to perform rotations on
+//added auto-find main camera
+//added un-initialized state, where script will do nothing
+using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour {
+
+	public Camera m_Camera;
+	GameObject myContainer;	
+	
+	void Start(){
+		m_Camera = Camera.main;
+		
+		myContainer = new GameObject();
+		myContainer.name = "GRP_"+transform.gameObject.name;
+		myContainer.transform.position = transform.position;
+		transform.parent = myContainer.transform;
+
+		
+		myContainer.transform.LookAt(m_Camera.transform, Vector3.up);
+	}
+	
+	
+	void Update(){
+		myContainer.transform.LookAt(m_Camera.transform, Vector3.up);
+	}
+}
