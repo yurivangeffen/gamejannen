3744ad37512805b343dc06b2f695a71e82f3b7cb
Jannes555 <jannes_elings@hotmail.com>
2015-05-09 18:49:49 +0200

    Menu

---
+++ Assets/Scripts/RandomYRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomYRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.Rotate(Vector3.up, 360 * Random.value);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
